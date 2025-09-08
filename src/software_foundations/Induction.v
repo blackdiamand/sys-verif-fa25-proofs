ed0231e7632071459fb2c3ef4b201d15914f7e5f
blackdiamand <34006971+blackdiamand@users.noreply.github.com>
2025-09-07 22:48:06 -0500

    induction1

--- src/software_foundations/Induction.v
+++ src/software_foundations/Induction.v
@@ -242,17 +242,29 @@ Proof.
 Theorem mul_0_r : forall n:nat,
   n * 0 = 0.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  induction n as [| n' IHn'].
+  - simpl. reflexivity.
+  - simpl. rewrite IHn'. reflexivity. 
+Qed.
 
 Theorem plus_n_Sm : forall n m : nat,
   S (n + m) = n + (S m).
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros n m.
+  induction n.
+  - simpl. reflexivity.  
+  - simpl. rewrite IHn. reflexivity.
+Qed.
 
 Theorem add_comm : forall n m : nat,
   n + m = m + n.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros n m.
+  induction n.
+  - simpl. rewrite <- plus_n_O. reflexivity.
+  - simpl. rewrite IHn. rewrite plus_n_Sm. reflexivity.
+Qed.
+
 
 Theorem add_assoc : forall n m p : nat,
   n + (m + p) = (n + m) + p.
